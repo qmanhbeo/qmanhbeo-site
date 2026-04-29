477450ee63934f31c210b9fe8a53b22d07ff788f
qmanhbeo <manh.nq12082003@gmail.com>
2026-04-30 00:02:52 +0100

    Fix: add sagrada-familia note to index

--- content/notes/index.ts
+++ content/notes/index.ts
@@ -1,5 +1,6 @@
 import type { NoteEntry } from "@/content/entries"
 import noteManhMemorySystemFrustrationStocks from "./note-manh-memory-system-frustration-stocks"
 import noteRemoteWslVivaLaVida from "./note-remote-wsl-viva-la-vida"
+import noteSagradaFamiliaGaia from "./note-sagrada-familia-gaia"
 
-export const noteEntries = [noteManhMemorySystemFrustrationStocks, noteRemoteWslVivaLaVida] satisfies NoteEntry[]
+export const noteEntries = [noteManhMemorySystemFrustrationStocks, noteRemoteWslVivaLaVida, noteSagradaFamiliaGaia] satisfies NoteEntry[]
